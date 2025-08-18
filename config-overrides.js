7cae7e9115a35ed333a66db7492eae0e6c900113
Master Pump <devweb4.contact@gmail.com>
2025-08-19 00:50:08 +0200

    Update config-overrides.js

--- config-overrides.js
+++ config-overrides.js
@@ -3,6 +3,9 @@ const webpack = require("webpack");
 module.exports = function override(config, env) {
   config.resolve = {
     ...config.resolve,
+    extensionAlias: {
+      ".js": [".js", ".ts", ".tsx"]
+    },
     fallback: {
       ...(config.resolve ? config.resolve.fallback : {}),
       crypto: require.resolve("crypto-browserify"),
